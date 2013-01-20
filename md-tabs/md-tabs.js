fdf7fead3f4154c0b1635f6d60faec02898648e7
tacamy <takami.yamada@gmail.com>
2013-01-21 00:04:49 +0900

    メソッド名と記述位置を変更

--- md-tabs/md-tabs.js
+++ md-tabs/md-tabs.js
@@ -1,48 +1,41 @@
-(function($) { // カプセル化
+(function($) {
 
 	$.fn.tabs = function(options) {
+		
+		/**
+		 * Method
+		 */
+		var _hidePanel = function($panels) {
+			$panels.hide();
+		};
+		
+		var _showPanel = function($navs) {
+			var $activeNav = $navs.filter(".active");
+			var href = $activeNav.attr("href");
+			$(href).slideDown();
+		};
 
+		/**
+		 * Constructor
+		 */
 		return this.each(function() {
-
-			// タブとパネルの要素取得
 			var $navs = $(this).find(".md-tabsNav a");
 			var $panels = $(this).find(".md-tabsPanel");
 
-			// JSオフ対策で、パネル非表示をJSで行う
-			hidePanel($panels);
-			showPanel($navs);
+			// JSオフでも内容が見れるように、初期のパネル表示/非表示の制御をJSで行う
+			_hidePanel($panels);
+			_showPanel($navs);
 
-			// タブクリック時の動作
 			$navs.on("click", function(ev) {
-
-				// aのデフォルトイベントを殺す
 				ev.preventDefault();
 
-				// パネルを非表示
-				hidePanel($panels);
+				_hidePanel($panels);
 
-				// タブの.activeを全て解除
 				$navs.removeClass("active");
-
-				// クリックしたタブに.activeをつける
 				$(this).addClass("active");
 				
-				// アクティブタブのhref先のパネルを表示
-				showPanel($navs);
+				_showPanel($navs);
 			});
 		});
 	};
-
-	// パネルを非表示
-	function hidePanel($panels) {
-		$panels.hide();
-	}
-
-	// アクティブタブのhref先のパネルを表示
-	function showPanel($navs) {
-		var $activeNav = $navs.filter(".active");
-		var href = $activeNav.attr("href");
-		$(href).slideDown();
-	}
-
 }(jQuery));
