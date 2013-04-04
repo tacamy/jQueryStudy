e0c762f3f7dd7a0c37eade32a25fec2f8759f2ed
tacamy <takami.yamada@gmail.com>
2013-04-04 17:00:52 +0900

    First Commit

---
+++ jquery.checkForm/README.md
@@ -0,0 +1,54 @@
+# jquery.checkForm.js
+
+- フォームの必須項目が埋まっていなければ、送信ボタンがdisabledになって送信できません。
+- エラー箇所のブロックにClass名を振って、スタイルを変更できます。
+- エラーの内容を、エラーメッセージのボックスにまとめて表示します。
+
+## デモ - Demo
+
+[https://dl.dropbox.com/u/368057/study/jquery.checkForm/index.html](https://dl.dropbox.com/u/368057/study/jquery.checkForm/index.html)
+
+## 使い方 - Usage
+
+### サンプル
+
+	<head>
+	<script src="./jquery.heightAlign.js"></script>
+	<script>
+	$(function() {
+	  $('[data-type="form"]').checkForm();
+	});
+	</script>
+	</head>
+	<body>
+	<form action="#" method="POST" data-type="form">
+	  <div data-type="required">
+	    <input type="text" name="text" value="">
+	  </div>
+	  <div data-type="required">
+	    <ul>
+	      <li><label><input type="checkbox" name="checkbox" value="1">1</label></li>
+	      <li><label><input type="checkbox" name="checkbox" value="2">2</label></li>
+	    </ul>
+	  </div>
+	  <div data-type="required">
+	    <ul>
+	      <li><label><input type="radio" name="radio" value="1">1</label></li>
+	      <li><label><input type="radio" name="radio" value="2">2</label></li>
+	    </ul>
+	  </div>
+	  <input data-type="submit" type="submit">
+	</form>
+	</body>
+
+### オプション
+
+- errorItemClassName : エラー箇所のブロックにつけるクラス名を指定できます。
+- errorMessageClassName : エラー内容を表示するボックスのクラス名を指定できます。
+
+## 対応ブラウザ - Browser
+
+- Internet Explorer 6, 7, 8, 9, 10
+- Firefox
+- Safari
+- Chrome
---
+++ jquery.checkForm/jquery.checkForm.js
@@ -0,0 +1,111 @@
+(function($) {
+
+  $.fn.checkForm = function(options) {
+    var ERR = -1;
+    var ERRMSG = [
+      '1ブロック目のエラーメッセージ',
+      '2ブロック目のエラーメッセージ',
+      '3ブロック目のエラーメッセージ'
+    ];
+
+    options = $.extend({
+      errorItemClassName: 'error',
+      errorMessageClassName: 'errorMessage'
+    }, options);
+
+    return this.each(function() {
+      var $self = $(this);
+      var $submit = $('[data-type="submit"]');
+      var $requiredList = $('[data-type="required"]');
+      var $errorMessageBox = $('<div></div>')
+        .addClass(options.errorMessageClassName)
+        .prependTo($self);
+      var $errorMessageLists = $('<ul></ul>')
+        .appendTo($errorMessageBox);
+      var $errorMessageArray = [];
+
+      // 初期化
+      $self.submit(function(e) {
+        e.preventDefault();
+      });
+      $submit.attr('disabled', 'disabled');
+
+      // form以下のすべてのinput要素のイベントを監視
+      $self.on('change', 'input', function() {
+
+        // エラーメッセージの配列を空にして初期化
+        $errorMessageArray.length = 0;
+
+        $.each($requiredList, function(i) {
+          var $self = $(this);
+          var $elem = $self.find('input');
+          var rc = null;
+
+          if($elem.is(':text')) {
+            rc = checkText($elem);
+          } else if($elem.is(':checkbox')) {
+            rc = checkChecked($elem);
+          } else if($elem.is(':radio')) {
+            rc = checkChecked($elem);
+          };
+
+          if(rc === ERR) {
+            $self.addClass(options.errorItemClassName);
+            $errorMessageArray.push(ERRMSG[i]);
+          } else {
+            $self.removeClass(options.errorItemClassName);
+          }
+        });
+
+        // エラーが1つでもある場合は、
+        if($self.find('.' + options.errorItemClassName).length) {
+
+          // submitをdisabled状態にして、
+          $submit.attr('disabled', 'disabled');
+
+          // エラーメッセージを表示
+          $errorMessageLists.empty();
+          $.each($errorMessageArray, function(i) {
+            $('<li></li>')
+              .text($errorMessageArray[i])
+              .appendTo($errorMessageLists);
+          });
+          $errorMessageBox.show();
+        
+        // エラーがなければ、
+        } else {
+
+          // submitのdisabledを外して、
+          $submit.removeAttr('disabled');
+
+          // エラーメッセージを非表示
+          $errorMessageBox.hide();
+        }
+      });
+    });
+
+    // 1文字も入力されていなければエラーを返す
+    function checkText($elem) {
+      if($elem.val() === '') {
+        return ERR;
+      } else {
+        return this;
+      }
+    }
+
+    // 1つもチェックされていなければエラーを返す
+    function checkChecked($elem) {
+      var cnt = 0;
+      $elem.each(function() {
+        if($(this).prop('checked') === true) {
+          cnt++;
+        }
+      });
+      if(cnt === 0) {
+        return ERR;
+      } else {
+        return this;
+      }
+    }
+  };
+}(jQuery));
