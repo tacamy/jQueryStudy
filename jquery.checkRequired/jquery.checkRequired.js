d722efb450368214a0b2b6ba7c12646f12bbcf02
tacamy <takami.yamada@gmail.com>
2013-04-10 20:13:00 +0900

    エラーぽく見せないように仕様変更

--- jquery.checkRequired/jquery.checkRequired.js
+++ jquery.checkRequired/jquery.checkRequired.js
@@ -1,11 +1,8 @@
 (function($) {
 
   $.fn.checkRequired = function(options) {
-    // const
-    var ERROR = -1;
 
     options = $.extend({
-      errorItemClassName: 'error',
       errorMessageClassName: 'errorMessage'
     }, options);
 
@@ -13,76 +10,71 @@
       var $form = $(this);
       var $submit = $('[data-cr-role="submit"]');
       var $requiredItems = $('[data-cr-check="required"]');
-      var $errorMessage = $('<div class="' + options.errorMessageClassName + '">未入力項目があります。</div>')
+      var $errorMessage = $('<div class="' + options.errorMessageClassName + '"></div>');
+      var errorCount;
 
-      // init
-      $form.submit(function(e) {
-        e.preventDefault();
-      });
-      changeDisabled($submit, 'disabled');
+      $form.on('submit', preventDefault);
+      errorCount = isInputedRequiredField($requiredItems);
+      switchSubmitButton(errorCount, $submit, $errorMessage);
 
-      // delegate
       $form.on('change', 'input', function() {
-
-        $requiredItems.each(function() {
-          var $requiredItem = $(this);
-          var $inputItems = $requiredItem.find('input');
-          var rc = null;
-
-          if ($inputItems.is(':text')) {
-            rc = checkText($inputItems);
-          } else if ($inputItems.is(':checkbox') || $inputItems.is(':radio')) {
-            rc = checkChecked($inputItems);
-          };
-
-          if (rc === ERROR) {
-            $requiredItem.addClass(options.errorItemClassName);
-          } else {
-            $requiredItem.removeClass(options.errorItemClassName);
-          }
-        });
-
-        if ($form.find('.' + options.errorItemClassName).length) {
-          changeDisabled($submit, 'disabled');
-          $submit.before($errorMessage.show());
-        } else {
-          changeDisabled($submit);
-          $errorMessage.hide();
-        }
+        errorCount = isInputedRequiredField($requiredItems);
+        switchSubmitButton(errorCount, $submit, $errorMessage);
       });
     });
 
-    // 1文字も入力されていなければエラーを返す
-    function checkText($elem) {
-      if ($elem.val() === '') {
-        return ERROR;
-      } else {
-        return this;
-      }
+    function preventDefault(e) {
+      e.preventDefault();
     }
 
-    // 1つもチェックされていなければエラーを返す
-    function checkChecked($elem) {
-      var cnt = 0;
-      $elem.each(function() {
-        if ($(this).prop('checked') === true) {
-          cnt++;
-        }
+    function isInputedRequiredField($requiredItems) {
+      errorCount = 0;
+
+      $requiredItems.each(function() {
+        var $requiredItem = $(this);
+        var $inputItems = $requiredItem.find('input');
+        var error;
+
+        if ($inputItems.is(':text')) {
+          error = hasAnyTextInput($inputItems);
+        } else if ($inputItems.is(':checkbox') || $inputItems.is(':radio')) {
+          error = isChecked($inputItems);
+        };
+
+        if (error) errorCount++;
       });
-      if (cnt === 0) {
-        return ERROR;
-      } else {
-        return this;
-      }
+
+      return errorCount;
     }
 
-    // disabled属性の切替
-    function changeDisabled($submit, state) {
-      if (state === 'disabled') {
-        $submit.attr('disabled', 'disabled');
+    function switchSubmitButton(errorCount, $submit, $errorMessage) {
+      if (errorCount) {
+        $submit
+          .attr('disabled', 'disabled')
+          .before(insertErrorMassage($errorMessage, errorCount))
+          .closest('form').off('submit', preventDefault);
       } else {
         $submit.removeAttr('disabled');
+        $errorMessage.hide();
       }
-    };
+    }
+
+    function insertErrorMassage($errorMessage, errorCount) {
+      return $errorMessage
+        .text('必須項目が入力されていません（残り' + errorCount + '個）')
+        .show();
+    }
+
+    function hasAnyTextInput($elem) {
+      return ($elem.val() === '') ? true : false;
+    }
+
+    function isChecked($elem) {
+      var $checked = $elem.filter(function() {
+        return $(this).prop('checked') === true;
+      });
+
+      return ($checked.length === 0) ? true : false;
+    }
   };
 }(jQuery));
